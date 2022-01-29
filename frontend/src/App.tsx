959d1bc0bcad29784595f0c2720ce0c065763a02
andrey zobkov <andrey.zobkov@tobania.be>
2022-01-29 23:55:21 +0300

    fixed routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route, Router } from 'react-router-dom';
+import { Redirect, Route, Router } from 'react-router-dom';
 import Registration from './pages/Registration/registration';
 import Login from './pages/Login/Login';
 import { CollectionView } from './pages/ReadCollection/CollectionView';
@@ -10,27 +10,26 @@ import { GuardedRouteComponent } from './components/GuardedRoute/guarded-route.c
 import { CollectionForm } from './pages/addCollection/collection-form.component';
 import { EditCollectionForm } from './pages/addCollection/edit-collection-form.component';
 
-// localhost:4200/collection/read/1
-
 export function App() {
   return (
     <Router history={appHistory}>
-      <Route path="/collection/read/:page/:limit"
-             component={CollectionsPage}/>
       <GuardedRouteComponent path="/collection/add"
                              component={CollectionForm}/>
       <GuardedRouteComponent path="/collection/edit/:id"
                              component={EditCollectionForm}/>
-      <Route path="/collection/:id"
-             exact={true}
+      <Route path="/collection/all/:page/:limit"
+             component={CollectionsPage}/>
+      <Route path="/collection/read/:id"
+             exact
              component={CollectionView}/>
       <Route path="/"
-             exact={true}
-             component={() => <div>lalalallalal</div>}/>
+             exact
+             component={() => <Redirect to="/collection/all/0/0"/>}/>
       <Route path="/login"
-             exact={true}
+             exact
              component={Login}/>
       <Route path="/registration"
+             exact
              component={Registration}/>
     </Router>
   );
